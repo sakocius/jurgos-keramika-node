ec0849317d253b265b0b13a5e4c7c19082ef188d
tomass <tomas.sakocius@asseco.lt>
2026-02-18 11:01:51 +0200

    Fix nodemailer import syntax in server.js and ensure it is listed as a dependency in package.json

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const mysql = require('mysql2/promise');
 const cors = require('cors');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-import nodemailer from 'nodemailer';
+const nodemailer = require('nodemailer');
 
 const app = express();
 
