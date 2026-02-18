7b48eb0e958cb346d5abd092677cf53b53621e4d
tomass <tomas.sakocius@asseco.lt>
2026-02-18 11:24:00 +0200

    Add IPv4 force option to nodemailer transport configuration

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
   secure: false,
+  family: 4,               // 👈 FORCE IPv4
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
